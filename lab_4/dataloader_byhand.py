4c601435782c6502acb405a0f2415c5eeeb62c6c
Anzor Magomedagaev <anzor.m13@gmail.com>
2025-11-29 04:03:35 +0400

    Перевёл докстринги и выводы на русский для удобства

--- lab_4/dataloader_byhand.py
+++ lab_4/dataloader_byhand.py
@@ -15,7 +15,7 @@ from torch.utils.data import Dataset, DataLoader
 
 
 def prepare_data(dataset_path, sent_col_name, label_col_name):
-    """ 读出tsv中的句子和标签 """
+    """ Чтение предложений и меток из tsv файла """
     file_path = os.path.join(dataset_path, "train.tsv")
     data = pd.read_csv(file_path, sep="\t")
     X = data[sent_col_name].values
@@ -24,7 +24,7 @@ def prepare_data(dataset_path, sent_col_name, label_col_name):
 
 
 class Language:
-    """ 根据句子列表建立词典并将单词列表转换为数值型表示 """
+    """ Создание словаря на основе списка предложений и преобразование слов в числовое представление """
     def __init__(self):
         self.word2id = {}
         self.id2word = {}
@@ -48,7 +48,7 @@ class Language:
 
 
 class ClsDataset(Dataset):
-    """ 文本分类数据集 """
+    """ Датасет для классификации текста """
     def __init__(self, sents, labels):
         self.sents = sents
         self.labels = labels
@@ -61,7 +61,7 @@ class ClsDataset(Dataset):
 
 
 def collate_fn(batch_data):
-    """ 自定义一个batch里面的数据的组织方式 """
+    """ Кастомная функция для организации данных внутри батча """
     batch_data.sort(key=lambda data_pair: len(data_pair[0]), reverse=True)
 
     sents, labels = zip(*batch_data)
@@ -73,8 +73,8 @@ def collate_fn(batch_data):
 
 
 def get_wordvec(word2id, vec_file_path, vec_dim=50):
-    """ 读出txt文件的预训练词向量 """
-    print("开始加载词向量")
+    """ Загрузка предобученных word vectors из txt файла """
+    print("Начинаю загрузку векторов слов")
     word_vectors = torch.nn.init.xavier_uniform_(torch.empty(len(word2id), vec_dim))
     word_vectors[0, :] = 0  # <pad>
     found = 0
@@ -85,9 +85,9 @@ def get_wordvec(word2id, vec_file_path, vec_dim=50):
             if splited[0] in word2id:
                 found += 1
                 word_vectors[word2id[splited[0]]] = torch.tensor(list(map(lambda x: float(x), splited[1:])))
-            if found == len(word2id) - 1:  # 允许<unk>找不到
+            if found == len(word2id) - 1:  # Разрешаем не найти <unk>
                 break
-    print("总共 %d个词，其中%d个找到了对应的词向量" % (len(word2id), found))
+    print("Всего %d слов, из них %d найдены соответствующие векторы" % (len(word2id), found))
     return word_vectors.float()
 
 
@@ -103,10 +103,10 @@ def make_dataloader(dataset_path="../dataset/kaggle-movie-review", sent_col_name
     X = X_language.transform(X)
 
     word_vectors = get_wordvec(X_language.word2id, vec_file_path=vec_file_path, vec_dim=50)
-    # 总共 18229个词，其中12769个找到了对应的词向量 word_vectors = get_wordvec(X_language.word2id,
+    # Всего 18229 слов, из них 12769 найдены соответствующие векторы word_vectors = get_wordvec(X_language.word2id,
     # vec_file_path=r"F:\NLP-pretrained-model\glove.twitter.27B\glove.twitter.27B.50d.txt", vec_dim=50)
 
-    # 测试
+    # Тест
     # print(X[:2])
     # X_id = X_language.transform(X[:2])
     # print(X_language.transform(X_id, reverse=True))
